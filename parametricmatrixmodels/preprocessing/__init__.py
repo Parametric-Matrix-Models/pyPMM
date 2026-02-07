783a50cf602bdf33544751a4b9169c030a9146c4
Patrick Cook <qphysicscook@gmail.com>
2026-02-06 23:03:00 -0500

    added new scalers

--- parametricmatrixmodels/preprocessing/__init__.py
+++ parametricmatrixmodels/preprocessing/__init__.py
@@ -1,7 +1,6 @@
 # rexport all
 from .minmax_scaler import MinMaxScaler
+from .mixed_scaler import MixedScaler
+from .robust_scaler import RobustScaler
 from .scaler import Scaler
 from .standard_scaler import StandardScaler
-
-# from .robust_scaler import RobustScaler
-# from .normalizer import Normalizer
